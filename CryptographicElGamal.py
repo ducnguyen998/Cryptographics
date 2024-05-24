73dc5390459f94cbf4e91a14d2bf05633573b6a8
ducnguyen.998 <duc18041998@gmail.com>
2024-05-25 01:10:34 +0700

    ElGamal, Elliptic Curve, RSA added

--- CryptographicElGamal.py
+++ CryptographicElGamal.py
@@ -1,34 +1,135 @@
-import CryptoAlgorithms as crypto
-import PrimeHelper as primeHelper
 import random as rd
 
-_bits_min = 1025
+from Helper import (
+    PrimeHelper
+)
 
-### Choose p is a prime
+class ElGamalCryptoProvider:
 
-checker = primeHelper.MillerRabinChecker()
+    def __init__(self):
+        self.helper = PrimeHelper()
 
-_b = False
-_i = 0
-while _b is False:
-    _n = rd.randint(2 ** 2048, 2 ** 4096)
-    _b = checker.get_probably_prime(_n, 2 ** 1024)
-    _i += 1
-    print(f'{_n} is prime : {_b} - {_i}')
+    def generate_cryptographic(self, bm=16, bM=17):
+        p = self.helper.create(bits_min=bm, bits_max=bM, k=64)
+        #print(f'p={p}')
+        a = p - rd.randint(2 ** (bm-2), 2 ** (bm-1))
+        #print(f'a={a}')
+        #
+        K_ii = a
+        #
+        alpha = self.helper.find_primitive_root(p)
+        #print(f'alpha={alpha}')
+        beta  = self.helper.modular_exponentiation(alpha, a, p)
+        #print(f'beta={beta}')
+        #
+        K_i = (p, alpha, beta)
+        # 
+        return ElGamalCrypto(K_i, K_ii)
 
-print(f'{_n} is prime : {_b}')
+class ElGamalCrypto:
+    def __init__(self, K_i, K_ii):
+        self.K_i = K_i
+        self.K_ii = K_ii
+        self.a = K_ii
+        self.p, self.alpha, self.beta = K_i
+        self.helper = PrimeHelper()
+    #
+    # Encrypt by self.K_i (public key)
+    #
+    def encrypt(self, x):
+        p, alpha, beta = self.p, self.alpha, self.beta
+        k = rd.randint(2, self.p - 1)
+        c1 = self.helper.modular_exponentiation(alpha, k, p)
+        c2 = self.helper.modular_exponentiation( beta, k, p) * x % p
+        return c1, c2
+    #
+    # Decrypt by partner K_ii & p 
+    #
+    def decrypt(self, y):
+        a = self.a
+        p = self.p
+        c1, c2 = y
+        x = c2 * self.helper.mod_inverse(c1 ** a, p) % p
+        return x
+    
+    #
+    # Decrypt by partner K_ii & p 
+    #
+    def decrypt_by_partner(self, y, K_ii_partner, p_partner):
+        a = K_ii_partner
+        p = p_partner
+        c1, c2 = y
+        x = c2 * self.helper.mod_inverse(c1 ** a, p) % p
+        return x
+    #
+    # Encrypt by partner K_i (public key)
+    #
+    def encrypt_by_partner(self, x, K_i_partner):
+        p, alpha, beta = K_i_partner
+        k = rd.randint(2, self.p - 1)
+        c1 = self.helper.modular_exponentiation(alpha, k, p)
+        c2 = self.helper.modular_exponentiation( beta, k, p) * x % p
+        return c1, c2
+    
+    def __str__(self) -> str:
+        return f'ElGamalCrypto: K_i = [p={self.p}, alpha={self.alpha}, beta={self.beta}], K_ii = [a={self.a}]'
 
-### Create Z*p
+class ElGamalCryptoSignature:
+    def __init__(self, cryptographic):
+        self.helper = PrimeHelper()
+        self.K_i, self.K_ii = cryptographic.K_i, cryptographic.K_ii
+        self.a = self.K_ii
+        self.p, self.alpha, self.beta = self.K_i
 
-### Choose a in Z*p
+        # Choose a k value, that have inverse value in mod(p-1)
+        for i in range(2, self.p - 1):
+            if self.helper.mod_inverse(i, self.p - 1) != None:
+                self.k = i
+                break
 
-#a = rd.randint(_n / 2, _n - 1)
-#print (a)
+        if self.k is None:
+            raise ValueError(f"self.k is None")
 
-### K' = a
+        self.gamma = self.helper.modular_exponentiation(self.alpha, self.k, self.p)
 
-### Find K" = (p, alpha, beta)
+    def sign(self, x):
+        self.delta = (x - self.a * self.gamma) * self.helper.mod_inverse(self.k, self.p - 1) % (self.p - 1)
+        print(f'Sign : (hx, (gamma, delta)) = ({x},({self.gamma}, {self.delta}))')
+    def verify(self, x, K_i_partner, gamma, delta):
+        p, alpha, beta = K_i_partner
+        v1 = self.helper.modular_exponentiation((beta ** gamma) * (gamma ** delta), 1, p)
+        v2 = self.helper.modular_exponentiation(alpha, x, p)
+        print(f'v1={v1}, v2={v2}')
+        return v1 == v2
 
-### - alpha : 
+def main():
+    cryptoProvider = ElGamalCryptoProvider()
 
-### - beta  : alpha^a mode p
+    # He mat 1
+    r1 = cryptoProvider.generate_cryptographic()
+    print(r1)
+    print('-' * 50)
+
+    # He mat 2
+    r2 = cryptoProvider.generate_cryptographic()
+    print(r2)
+    print('-' * 50)
+
+    # Crypting
+
+    x = 12345
+    y = r1.encrypt_by_partner(x, r1.K_i)
+    m = r1.decrypt(y)
+
+    print(f'x={x} : encrypt({y}) , decrypt({m})')
+
+    # Signature
+    s1 = ElGamalCryptoSignature(r1)
+    s2 = ElGamalCryptoSignature(r2)
+
+    s2.sign(x)
+    verify = s2.verify(x, s2.K_i, s2.gamma, s2.delta)
+    print(f'verification : {verify}')
+
+if __name__ == '__main__':
+    main()
---
+++ Helper.py
@@ -0,0 +1,104 @@
+
+#
+## Miller-Rabin algorithm
+#
+
+import random
+
+class MillerRabinChecker:  
+    def get_probably_prime(self, n, k):
+        #
+        if n == 2 or n == 3:
+            return True
+        if n <= 1 or n % 2 == 0:
+            return False
+        # n-1 --> 2^s * d
+        s = 0
+        d = n - 1
+        while d % 2 == 0:
+            d //= 2
+            s += 1
+        #
+        for _ in range(k):
+            a = random.randint(2, n - 2)
+            if self.is_composite(a, n, d, s):
+                return False
+        return True
+    def is_composite(self, a, n, d, s):
+        x = pow(a, d, n)
+        if x == 1 or x == n-1:
+            return False
+        for _ in range(s-1):
+            x = pow(x, 2, n)
+            if x == n - 1:
+                return False
+        return True
+        
+class PrimeHelper:
+    def __init__(self):
+        self._checker = MillerRabinChecker()
+    def create(self, bits_min=1024, bits_max=2048, k=64):
+        _b = False
+        _i = 0
+        while _b is False:
+            _n = random.randint(2 ** bits_min, 2 ** bits_max)
+            _b = self._checker.get_probably_prime(_n, k)
+            _i += 1
+        print(f'{_n} is prime : {_b} - {_i}')
+        return _n
+    def _prime_factors(self, n):
+        factors = set()
+        while n % 2 == 0:
+            factors.add(2)
+            n //= 2
+        for i in range(3, int(n**0.5) + 1, 2):
+            while n % i == 0:
+                factors.add(i)
+                n //= i
+        if n > 2:
+            factors.add(n)
+        return factors
+    def _is_primitive_root(self, g, p):
+        p_minus_1 = p - 1
+        factors = self._prime_factors(p_minus_1)
+        for factor in factors:
+            if pow(g, p_minus_1 // factor, p) == 1:
+                return False
+        return True
+    def find_primitive_root(self, p):
+        # be sure p is a prime
+        for g in range(2, p):
+            if self._is_primitive_root(g, p):
+                return g
+        return None
+    def modular_exponentiation(self, base, exponent, modulus):
+        '''
+        Return : base ** exponent % modulus
+        '''
+        result = 1
+        base = base % modulus  # Nếu base >= modulus
+
+        while exponent > 0:
+            if (exponent % 2) == 1:  # Nếu exponent là số lẻ
+                result = (result * base) % modulus
+            exponent = exponent >> 1  # Chia exponent cho 2
+            base = (base * base) % modulus
+
+        return result
+    #
+    #
+    #
+    def extended_gcd(self, a, b):
+        if a == 0:
+            return b, 0, 1
+        gcd, x1, y1 = self.extended_gcd(b % a, a)
+        x = y1 - (b // a) * x1
+        y = x1
+        return gcd, x, y
+    def mod_inverse(self, a, p):
+        gcd, x, _ = self.extended_gcd(a, p)
+        if gcd != 1:
+            return None #raise ValueError(f"{a} không có số nghịch đảo modulo {p}")
+        else:
+            return x % p
+        
